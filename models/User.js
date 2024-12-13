033a5d8f8bc8b66f45a2e73a8d6611f98775c96b
Laxmisai-gunda <112396504+Laxmisai-gunda@users.noreply.github.com>
2024-12-13 20:34:41 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-// Define User schema
+ 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
